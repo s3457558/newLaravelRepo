5ce4444de6b12b2e0bc4c6ae1749fd5677f37467
s3457558 <s3457558@student.rmit.edu.au>
2017-08-12 13:14:40 +1000

    AOAOO

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -14,7 +14,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Booking</a></li>
+                <li><a href="http://54.252.194.171/booking">Booking</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
                 <li class="dropdown">
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'PagesController@home');
+Route::get('booking', 'Controller@showBooking');
