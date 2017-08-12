3744f1924c8275b40db360f36a43a552a956d81e
s3457558 <s3457558@student.rmit.edu.au>
2017-08-12 12:46:35 +1000

    WOW

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+@section('title', 'Home')
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Welcome to join us!</div>
+            {{--<div class="quote">Our Home page!</div>--}}
+        </div>
+    </div>
+@endsection
