9c3714866d65d10e6e23a87452b20ff72ebb3a50
Chunki Leung <s3444706@student.rmit.edu.au>
2017-08-12 15:14:01 +1000

    Test Commit 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="content">
-            <div class="title">Welcome to join us HEY THIS HAS CHANGED!yhnyjnyjyj</div>
+            <div class="title">Welcome to join us HEY THIS HAS CHANGED!gdsgsdfsafsasa</div>
             {{--<div class="quote">Our Home page!</div>--}}
         </div>
     </div>
