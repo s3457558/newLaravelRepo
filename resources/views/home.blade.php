3f7b515ccf6347eca108a160e5a41968c8ed30c3
Chunki Leung <s3444706@student.rmit.edu.au>
2017-08-15 15:32:41 +1000

    Test 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="content">
-            <div class="title">Home Page</div>
+            <div class="title">Home Pagefgdsgdsgdsg</div>
             <div class="quote">Our Home page!</div>
         </div>
     </div>
