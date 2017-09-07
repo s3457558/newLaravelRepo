b91299ccb6fb9b9b2356890ce9e91f4e0ba8b148
Jason Zhuang <zhuangkaizhi@gmail.com>
2017-09-07 16:33:14 +1000

    update

--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container">
         <div class="content">
-            <div class="title">
-                <h2>Price</h2>
-            </div>
+            {{--<div class="title">--}}
+                {{--<h2>Price</h2>--}}
+            {{--</div>--}}
 
            <table class="content display-tablecontainer" >
                <tr>
@@ -18,38 +18,36 @@
 
                <tr>
                    <td><img src="images/cars/holden_commodore_sport.png"width="300px"></td>
-                   <td><font face="Arial" size="4" color="#006400"><strong>Holden commodore</strong></font></td>
-                   <td>Sport</td>
-                   <td>$10 per hour</td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Holden commodore</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Sport Sedan</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>$14 per hour</strong></font></td>
                </tr>
                <tr>
                    <td><img src="images/cars/Hyundai_unveils_van.png"width="300px"></td>
-                   <td><font face="Arial" size="4" color="#000000"><strong>Hyundai unveils</strong></font></td>
-                   <td><font face="Arial" size="4" color="#000000"><strong>Van</strong></font></td>
-                   <td><font face="Arial" size="4" color="#000000"><strong>$12 per hour</strong></font></td>
-                   {{--<td>Van</td>--}}
-                   {{--<td>$12 per hour</td>--}}
+                   <td><font face="Arial" size="3" color="#000000"><strong>Hyundai unveils</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Van</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>$18 per hour</strong></font></td>
                </tr>
 
                <tr>
                    <td><img src="images/cars/mazda2_Hatchback.png"width="300px"></td>
-                   <td><font face="Arial" size="4" color="#006400"><strong>Mazda2</strong></font></td>
-                   <td>Hatchback</td>
-                   <td>$12 per hour</td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Mazda2</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Hatchback</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>$13 per hour</strong></font></td>
                </tr>
 
                <tr>
                    <td><img src="images/cars/mercedes_cla_coupe.png"width="300px"></td>
-                   <td><font face="Arial" size="4" color="#006400"><strong>Mercedes cla</strong></font></td>
-                   <td>Coupe</td>
-                   <td>$12 per hour</td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Mercedes cla</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Coupe</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>$17 per hour</strong></font></td>
                </tr>
 
                <tr>
                    <td><img src="images/cars/rangeover_autobiography_suv_sport.png"width="300px"></td>
-                   <td><font face="Arial" size="4" color="#006400"><strong>Rangeover autobiography</strong></font></td>
-                   <td>Sport SUV</td>
-                   <td>$12 per hour</td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Rangeover autobiography</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>Sport SUV</strong></font></td>
+                   <td><font face="Arial" size="3" color="#000000"><strong>$20 per hour</strong></font></td>
                </tr>
            </table>
         </div>
