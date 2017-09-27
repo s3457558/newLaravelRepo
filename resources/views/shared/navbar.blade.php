8edabc7443574dbc20f4a642e8260e547d4d5f3c
jacksonfraser <s3529589@student.rmit.edu.au>
2017-09-27 12:36:00 +1000

    changed button in nav bar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -43,7 +43,7 @@
                 @endif
 
                 @if(!\Illuminate\Support\Facades\Auth::check())
-                    <li> <a class="register" href="upload">JOIN US</a></li>
+                    <li> <a class="register" href="upload">REGISTER</a></li>
                     <li> <a href="login">LOG IN</a></li>
                 @endif
                 @if( Auth::check() && Auth::user()->isAdmin )
